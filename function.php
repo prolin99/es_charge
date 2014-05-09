44b644da9bcee13490a54dd77356ab31282acc32
prolin99 <prolin99@gmail.com>
2014-05-09 11:18:20 +0800

    tadtool

--- function.php
+++ function.php
@@ -5,10 +5,10 @@
 // $Id:$
 // ------------------------------------------------------------------------- //
 //引入TadTools的函式庫
-if(!file_exists(TADTOOLS_PATH."/tad_function.php")){
+if(!file_exists(XOOPS_ROOT_PATH."/modules/tadtools/tad_function.php")){
  redirect_header("http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50",3, _TAD_NEED_TADTOOLS);
 }
-include_once TADTOOLS_PATH."/tad_function.php";
+include_once XOOPS_ROOT_PATH."/modules/tadtools/tad_function.php";
 
 /********************* 自訂函數 *********************/
 //取得身份別
