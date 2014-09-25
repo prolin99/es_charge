a104029c428171ef5e9cd25dfc0289fb76b4ba21
prolin99 <you@example.com>
2014-09-25 15:50:57 +0800

    班級判別

--- ajax_decrease.php
+++ ajax_decrease.php
@@ -6,9 +6,9 @@
 // ------------------------------------------------------------------------- //
 /*-----------引入檔案區--------------*/
 
-
 include_once "header.php";
 include_once "function.php";
+
 /*-----------function區--------------*/
 //data: { item_id: item , class_id :class_id  , detail_id:detail_id , stud_id : stud_id , sit_num:sit_num , money:money , cause: cause ,need:need , other:other },
 $item_id = intval($_GET['item_id']) ;
@@ -20,18 +20,36 @@ $money = intval($_GET['money']) ;
 $cause = intval($_GET['cause']) ;
 $need = intval($_GET['need']) ;
 $other = intval($_GET['other']) ;
- 
+
+if ($cause==0){
+	$need=0 ;
+	$other=0 ;
+}
+
+if ($other==$cause)
+	$other=0 ;
+
+if ($need==0 )
+	$other=0 ;
 
 
 
 /*-----------執行動作判斷區----------*/
-if  ($stud_id  and $item_id  and $detail_id  ) {
-	if ($money >0 ){}
-		 $sql = " REPLACE  INTO   " . $xoopsDB->prefix("charge_decrease") . "  set   detail_id='$detail_id' , student_sn='$stud_id' ,curr_class_num ='$class_id' ,
-		  	decrease_dollar='$money , cause_chk ='$cause'  , item_id='$item_id' , sit_num='$sit_num'  , cause_other='$other' " ;
-		 $result = $xoopsDB->queryF($sql) or die($sql."<br>". mysql_error()); 
-	}else {
-		$sql =  "  delete   FROM " . $xoopsDB->prefix("charge_decrease") . "   where detail_id='$detail_id' and student_sn='$stud_id' and  curr_class_num ='$class_id'   " ;
-		$result = $xoopsDB->queryF($sql) or die($sql."<br>". mysql_error()); 
-	}
-}  
+//取得所在班級
+$my_class_id =  get_my_class_id($xoopsUser->uid() ) ;
+if (($my_class_id<>$class_id ) and  (!$isAdmin) ) 
+	die ;
+//是否在期限內
+if ( item_in_time($item_id) or ($isAdmin)  ) {
+	if  ($stud_id  and $item_id  and $detail_id  ) {
+		if ($money >0 ){
+			 $sql = " REPLACE  INTO   " . $xoopsDB->prefix("charge_decrease") . "  set   detail_id='$detail_id' , student_sn='$stud_id' ,curr_class_num ='$class_id' ,
+		  		decrease_dollar='$money' , cause_chk ='$need'  , item_id='$item_id' , sit_num='$sit_num'  , cause_other='$other' " ;
+		 	$result = $xoopsDB->queryF($sql) or die($sql."<br>". mysql_error()); 
+		}else {
+			$sql =  "  delete   FROM " . $xoopsDB->prefix("charge_decrease") . "   where detail_id='$detail_id' and student_sn='$stud_id' and  curr_class_num ='$class_id'   " ;
+			$result = $xoopsDB->queryF($sql) or die($sql."<br>". mysql_error()); 
+		}
+		//echo $sql ;
+	}  
+}
--- ajax_decrease_cause.php
+++ ajax_decrease_cause.php
@@ -13,22 +13,39 @@ include_once "header.php";
 //data: { item_id: item , class_id :class_id  , stud_id : stud_id , cause: cause },
 
 /*-----------執行動作判斷區----------*/
-$item_id = intval($_GET['item_id']) ;
-$class_id = intval($_GET['class_id']) ;
-$stud_id = intval($_GET['stud_id']) ;
-$cause = intval($_GET['cause']) ;
+$item_id = intval($_POST['item_id']) ;
+$class_id = intval($_POST['class_id']) ;
+$stud_id = intval($_POST['stud_id']) ;
 
-if  ( $stud_id  ) {
- 	//是否在期限內
- 	if ( item_in_time($item_id) or ($isAdmin)  ) {
+$mode = intval($_POST['mode']) ;
+
+//取得所在班級
+$my_class_id =  get_my_class_id($xoopsUser->uid() ) ;
+if (($my_class_id<>$class_id ) and  (!$isAdmin) ) 
+	die ;
+//是否在期限內
+if ( item_in_time($item_id) or ($isAdmin)  ) {
+
+	if  ( $stud_id  and  $mode ==0  ) {
+		$cause = intval($_POST['cause']) ;
+
 		//更新
 		$sql="update "  . $xoopsDB->prefix("charge_record") .   " set cause= '$cause'  where item_id='$item_id'  and student_sn='$stud_id'  ";
 		$result = $xoopsDB->queryF($sql) or die($sql."<br>". mysql_error()); 
 		//echo $sql ;
+		if ($cause ==0 ){
+			//去除申請補助的設定
+			$sql="update "  . $xoopsDB->prefix("charge_decrease") .   " set cause_chk= 0  where item_id='$item_id'  and student_sn='$stud_id'  ";
+			$result = $xoopsDB->queryF($sql) or die($sql."<br>". mysql_error()); 
+		}
 	}
-	if ($cause ==0 ){
-		//去除申請補助的設定
-		$sql="update "  . $xoopsDB->prefix("charge_decrease") .   " set cause_chk= 0  where item_id='$item_id'  and student_sn='$stud_id'  ";
+	if  ( $stud_id  and  $mode ==99  ) {
+		$myts =& MyTextSanitizer::getInstance();
+		$ps =$myts->htmlspecialchars($myts->addSlashes ($_POST['cause'])  )  ;		
+		//更新
+		$sql="update "  . $xoopsDB->prefix("charge_record") .   " set ps = '$ps'  where item_id='$item_id'  and student_sn='$stud_id'  ";
 		$result = $xoopsDB->queryF($sql) or die($sql."<br>". mysql_error()); 
+		
 	}
+	//echo $sql ;
 } 
