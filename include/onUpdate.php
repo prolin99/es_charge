48bd1a09fb6f48078cb95ce4e51cfc61addce40e
prolin99 <you@example.com>
2015-04-30 16:10:01 +0800

    加姓名欄

--- include/onUpdate.php
+++ include/onUpdate.php
@@ -4,10 +4,11 @@ function xoops_module_update_es_charge(&$module, $old_version) {
     GLOBAL $xoopsDB;
     
     if(!chk_add_cause()) go_update_add_cause();
+    if(!chk_add_name()) go_update_add_name();    
     return true;
 }
 
-
+//------單項減免說明--------------------------------------------------
 function chk_add_cause(){
   global $xoopsDB;
   $sql=" select cause_other  from ".$xoopsDB->prefix("charge_decrease");
@@ -26,5 +27,20 @@ function go_update_add_cause(){
      $xoopsDB->queryF($sql)  ;
 }
 
+//--------開列清單加姓名，方便判別是否已轉校------------------------------------------------
+function chk_add_name(){
+  global $xoopsDB;
+  $sql=" select rec_name  from ".$xoopsDB->prefix("charge_record");
+  $result=$xoopsDB->query($sql);
+  if(empty($result)) return false;
+  return true;
+}
 
+function go_update_add_name(){
+  global $xoopsDB;
+
+     $sql=" ALTER TABLE  ".$xoopsDB->prefix("charge_record") ."  ADD `rec_name` VARCHAR( 20 )   NOT NULL   ";
+     $xoopsDB->queryF($sql)  ;
+
+ }
 ?>
