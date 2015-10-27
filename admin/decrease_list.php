2cd0afc3225ac6245dc4878108580338dfc5210c
prolin99 <prolin99@gmail.com>
2015-10-27 15:53:20 +0800

    補助標記

--- admin/decrease_list.php
+++ admin/decrease_list.php
@@ -6,17 +6,17 @@
 // ------------------------------------------------------------------------- //
 /*-----------引入檔案區--------------*/
 
-	
+
 include_once "header_admin.php";
- 
+
 include_once "../function.php";
- 
+
 include_once "../../tadtools/PHPExcel.php";
-require_once '../../tadtools/PHPExcel/IOFactory.php';    
- 
+require_once '../../tadtools/PHPExcel/IOFactory.php';
+
 /*-----------function區--------------*/
 //取得中文班名
-$class_list_c = es_class_name_list_c('long')  ;   
+$class_list_c = es_class_name_list_c('long')  ;
 
 /*-----------執行動作判斷區----------*/
 $item_id=empty($_REQUEST['item_id'])?"":$_REQUEST['item_id'];
@@ -24,27 +24,33 @@ $item_id=empty($_REQUEST['item_id'])?"":$_REQUEST['item_id'];
 
 if  ($item_id) {
 	$show_mode= 'all' ;
-	if ($_GET['show']== 'only') 
+	if ($_GET['show']== 'only')
 		$show_mode= 'only' ;
 
 	//細項名稱
 	$data['detail_list']=get_item_detail_list_name($item_id) ;
 
-	//$detail_id_array = array_keys($data['detail_list']) ; 
+	//$detail_id_array = array_keys($data['detail_list']) ;
 
 	//取得全部細項的收費
 	//$charge_array= get_detail_charge_dollars( $item_id) ;
-	
+
 	//全部已填的減免資料
 	$data['decase_list'] = get_all_decrease_list_item_array( $item_id , $show_mode ) ;
- 
+
 
 	$objPHPExcel = new PHPExcel();
 	$objPHPExcel->setActiveSheetIndex(0);  //設定預設顯示的工作表
 	$objActSheet = $objPHPExcel->getActiveSheet(); //指定預設工作表為 $objActSheet
-	$objActSheet->setTitle("依學生列表");  //設定標題		
+	$objActSheet->setTitle("依學生列表");  //設定標題
 	$objActSheet->getDefaultRowDimension()->setRowHeight(15);
- 	
+
+	//有補助的出現紅色
+	$Font_style = array(
+	    'font'  => array(
+	        'bold'  => true,
+	        'color' => array('rgb' => 'FF0000')
+	));
       //標題行
       	$objPHPExcel->setActiveSheetIndex(0)
             ->setCellValue('A1', 'NO.')
@@ -53,14 +59,14 @@ if  ($item_id) {
             ->setCellValue('D1', '性別')
             ->setCellValue('E1', '學生姓名') ;
             $col = 'E' ;
- 
+
 	foreach   (  $data['detail_list'] as $detail_id => $detail ) {
 		$col++ ;
 		$col_str =$col . '1' ;
 
 		$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str, $detail."減免") ;
 
-	}	
+	}
  	$col++ ;
 	$col_str = $col . '1' ;
         	$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , '減免原因') ;
@@ -69,49 +75,51 @@ if  ($item_id) {
 	$col_str = $col . '1' ;
         	$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , '連絡訊息') ;
 
- 
+
  	$row=1 ;
         //資料區
         foreach ($data['decase_list'] as $stud_id => $stud )  {
         	$row++ ;
         	//echo $col_str  ;
         	//$stud['sex']=1 ;
-        	$y = ($stud['curr_class_num'] /100)-1 ; 
-       	
+        	$y = ($stud['curr_class_num'] /100)-1 ;
+
        		$objPHPExcel->setActiveSheetIndex(0)
             		->setCellValue('A'.$row,$row-1)
             		->setCellValue('B'.$row , $class_list_c[$stud['curr_class_num']])
             		->setCellValue('C'.$row ,$stud['class_sit_num'])
             		->setCellValue('D'.$row, $stud['sex'])
             		->setCellValue('E'.$row, $stud['name']) ;
-         	$col = 'E' ;	
+         	$col = 'E' ;
 		foreach   (  $data['detail_list'] as $detail_id => $detail ) {
 			$col++ ;
 			$col_str = $col .$row ;
 
+			if ($stud['cause_chk'][ $detail_id])
+ 				$objPHPExcel->getActiveSheet()->getStyle("$col_str")->applyFromArray($Font_style);
 			$objPHPExcel->setActiveSheetIndex(0)->setCellValue("$col_str", $stud['dollar'][ $detail_id]) ;
-	
+
 		}
 		$col++ ;
 		$col_str = $col .$row ;
-		$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $stud['cause']) ;		
+		$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $stud['cause']) ;
 		//echo  $stud['cause'] ;
 		$col++ ;
 		$col_str = $col .$row ;
-		$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $stud['ps']) ;				
- 
+		$objPHPExcel->setActiveSheetIndex(0)->setCellValue($col_str , $stud['ps']) ;
+
 	}
- 
+
 	$objPHPExcel->getActiveSheet()->setTitle('student_decrease_list');
- 
+
 
 	header('Content-Type: application/vnd.ms-excel');
 	header('Content-Disposition: attachment;filename=decrease_' .$show_mode . '_' . date("mdHi").'.xlsx' );
 	header('Cache-Control: max-age=0');
- 
+
 	$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
 	//$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
 	$objWriter->save('php://output');
-	exit;		
+	exit;
 
 }
--- admin/decrease_list2.php
+++ admin/decrease_list2.php
@@ -6,17 +6,17 @@
 // ------------------------------------------------------------------------- //
 /*-----------引入檔案區--------------*/
 
-	
+
 include_once "header_admin.php";
- 
+
 include_once "../function.php";
- 
+
 include_once "../../tadtools/PHPExcel.php";
-require_once '../../tadtools/PHPExcel/IOFactory.php';    
- 
+require_once '../../tadtools/PHPExcel/IOFactory.php';
+
 /*-----------function區--------------*/
 //取得中文班名
-$class_list_c = es_class_name_list_c('long')  ;    
+$class_list_c = es_class_name_list_c('long')  ;
 
 /*-----------執行動作判斷區----------*/
 $item_id=empty($_REQUEST['item_id'])?"":$_REQUEST['item_id'];
@@ -24,28 +24,28 @@ $item_id=empty($_REQUEST['item_id'])?"":$_REQUEST['item_id'];
 
 if  ($item_id) {
 	$show_mode= 'all' ;
-	if ($_GET['show']== 'only') 
+	if ($_GET['show']== 'only')
 		$show_mode= 'only' ;
 
 	//細項名稱
 	$detail_list=get_item_detail_list_name($item_id) ;
 
-	//$detail_id_array = array_keys($data['detail_list']) ; 
+	//$detail_id_array = array_keys($data['detail_list']) ;
 
 	//取得全部細項的收費
 	$charge_array= get_detail_charge_dollars( $item_id) ;
-	
+
 	//全部已填的減免資料
 	$data['decase_list'] = get_all_decrease_list_item_kind_array( $item_id , $show_mode) ;
 
 	$objPHPExcel = new PHPExcel();
 	$objPHPExcel->setActiveSheetIndex(0);  //設定預設顯示的工作表
 	$objActSheet = $objPHPExcel->getActiveSheet(); //指定預設工作表為 $objActSheet
-	$objActSheet->setTitle("依項目減免");  //設定標題	
+	$objActSheet->setTitle("依項目減免");  //設定標題
  	$objActSheet->getDefaultRowDimension()->setRowHeight(15);
 
       //標題行
- 
+
         $objActSheet->setCellValue('A1', 'NO.');
         $objActSheet->setCellValue('B1', '類別');
         $objActSheet->setCellValue('C1', '班級');
@@ -55,41 +55,40 @@ if  ($item_id) {
         $objActSheet->setCellValue('G1', '應收') ;
         $objActSheet->setCellValue('H1', '減免') ;
         $objActSheet->setCellValue('I1', '實付') ;
-        $objActSheet->setCellValue('J1', '原因') ;
- 
- 
+        $objActSheet->setCellValue('J1', '補助') ;		
+        $objActSheet->setCellValue('K1', '原因') ;
+
+
  	$row=1 ;
  	//var_dump ($data['decase_list'])  ;
- 
+
         //資料區
         foreach ($data['decase_list'] as $stud_id => $stud )  {
         	$row++ ;
- 		//echo $row ;
-        	$y = ($stud['curr_class_num'] /100)-1 ; 
-       	
-       	$objActSheet->setCellValue('A'.$row,$row-1) ;
-       	$objActSheet->setCellValue('B'.$row, $detail_list[$stud['detail_id']]  ) ;
-	$objActSheet->setCellValue('C'.$row ,$class_list_c[$stud['curr_class_num']]) ;
-              $objActSheet->setCellValue('D'.$row ,$stud['class_sit_num']);
-              $objActSheet->setCellValue('E'.$row, $stud['sex']);
-              $objActSheet->setCellValue('F'.$row, $stud['name']) ;
-              $s_pay =$charge_array[$stud['detail_id']][$y] ; 
-              $objActSheet->setCellValue('G'.$row, $s_pay) ;
-              $objActSheet->setCellValue('H'.$row, $stud['decrease_dollar']+0) ;
-              $pay = $s_pay-$stud['decrease_dollar'] ;
-              $objActSheet->setCellValue('I'.$row, $pay) ;
+ 			//echo $row ;
+        	$y = ($stud['curr_class_num'] /100)-1 ;
 
-              if  ($stud['cause_other'])
-                $objActSheet->setCellValue('J'.$row , $decrease_cause[$stud['cause_other']] )    ;     
-              else   
-                if ($stud['cause_chk'])
-                    $objActSheet->setCellValue('J'.$row , $stud['cause_str']  ) ;        
-                else     
-                    
-     	      $objActSheet->setCellValue('J'.$row , '(不請補助)-' . $stud['cause_str']  ) ;		
-                
+       		$objActSheet->setCellValue('A'.$row,$row-1) ;
+       		$objActSheet->setCellValue('B'.$row, $detail_list[$stud['detail_id']]  ) ;
+			$objActSheet->setCellValue('C'.$row ,$class_list_c[$stud['curr_class_num']]) ;
+            $objActSheet->setCellValue('D'.$row ,$stud['class_sit_num']);
+            $objActSheet->setCellValue('E'.$row, $stud['sex']);
+            $objActSheet->setCellValue('F'.$row, $stud['name']) ;
+            $s_pay =$charge_array[$stud['detail_id']][$y] ;
+            $objActSheet->setCellValue('G'.$row, $s_pay) ;
+            $objActSheet->setCellValue('H'.$row, $stud['decrease_dollar']+0) ;
+            $pay = $s_pay-$stud['decrease_dollar'] ;
+            $objActSheet->setCellValue('I'.$row, $pay) ;
+			if ($stud['cause_chk'])
+				$objActSheet->setCellValue('J'.$row , 'v' ) ;
+
+              if  ($stud['cause_other'])	//有第二原因
+                $objActSheet->setCellValue('k'.$row , $decrease_cause[$stud['cause_other']] )    ;
+              else
+ 	      		$objActSheet->setCellValue('k'.$row ,  $stud['cause_str']  ) ;
+
         }
- 
+
 	header('Content-Type: application/vnd.ms-excel');
 	header('Content-Disposition: attachment;filename=dec_kind_'.$show_mode.date("mdHi").'.xlsx' );
 	header('Cache-Control: max-age=0');
@@ -97,6 +96,6 @@ if  ($item_id) {
 	$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
 	//$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
 	$objWriter->save('php://output');
-	exit;		
- 
+	exit;
+
 }
