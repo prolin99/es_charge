8e291d05d82685fa179ed9f32b28449af545e891
prolin99 <prolin99@gmail.com>
2016-12-09 11:17:05 +0800

    刪記錄檔

--- include/onUninstall.php
+++ include/onUninstall.php
@@ -24,26 +24,5 @@ function delete_directory($dirname) {
     return true;
 }
 
-//拷貝目錄
-function full_copy( $source="", $target=""){
-	if ( is_dir( $source ) ){
-		@mkdir( $target );
-		$d = dir( $source );
-		while ( FALSE !== ( $entry = $d->read() ) ){
-			if ( $entry == '.' || $entry == '..' ){
-				continue;
-			}
 
-			$Entry = $source . '/' . $entry;
-			if ( is_dir( $Entry ) )	{
-				full_copy( $Entry, $target . '/' . $entry );
-				continue;
-			}
-			copy( $Entry, $target . '/' . $entry );
-		}
-		$d->close();
-	}else{
-		copy( $source, $target );
-	}
-}
 ?>
